ea94b726300a240872732e6b1e4197e1a5b28c8f
GowthamTG <gowthamgopinath1@gmail.com>
2022-09-15 07:36:28 +0530

    Made Side Nav Responsive

---
+++ day2/dashboard/dashboard.js
@@ -0,0 +1,21 @@
+var openSideNav = () => {
+  const sideBarDisplay = document.getElementById("side-nav").style.display;
+  console.log(sideBarDisplay.length);
+  console.log(document.getElementById("side-nav").style.display);
+  console.log(document.getElementById("side-nav").style.position);
+  console.log(document.getElementById("side-nav").style.height);
+  console.log(document.getElementById("side-nav").style.zIndex);
+  const width = screen.width;
+  if (screen.width > 1080) {
+    document.getElementById("side-nav").style.display = "block";
+    document.getElementById("side-nav").style.position = "static";
+    document.getElementById("side-nav").style.height = "auto";
+  } else if (sideBarDisplay.length === 4 || sideBarDisplay.length === 0) {
+    document.getElementById("side-nav").style.display = "block";
+    document.getElementById("side-nav").style.position = "fixed";
+    document.getElementById("side-nav").style.height = "100%";
+    document.getElementById("side-nav").style.zIndex = 100;
+  } else {
+    document.getElementById("side-nav").style.display = "none";
+  }
+};
